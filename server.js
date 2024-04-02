9c1e282bf9afc142d1e788589c36d313d03e534d
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-01 21:32:42 -0700

    added destinations router

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require('./config/database'); // this MUST come after the require of dotenv
 
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
+var destinationRouter = require('./routes/destinations')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
+app.use('/', destinationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
