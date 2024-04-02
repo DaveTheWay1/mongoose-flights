b40c8d6856c4b5cfb818c19b975930dd540f3001
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-01 20:46:41 -0700

    added functionality to display all, to display add flight page, to create the and save the flight, and to show a single flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,2 +1,29 @@
 const Flight = require('../models/flight')
 
+module.exports = {
+    index,
+    new:newFlight,
+    create,
+    show
+}
+
+async function index(req,res){
+    const flights = await Flight.find({});
+    res.render('flights', {flights})
+}
+
+async function newFlight(req,res){
+    res.render('flights/new')
+}
+
+async function create(req,res){
+    console.log(req.body)
+    await Flight.create(req.body);
+    res.redirect('/flights')
+}
+
+async function show(req,res){
+    console.log(req.params.id);
+    const flight = await Flight.findById(req.params.id);
+    res.render('flights/show', {flight})
+}
