37aa0983ea47ac4ada5e5121c9f198e85381f272
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-01 20:51:12 -0700

    created schema for flights and schema for destinations where destinations have been embedded

--- models/flight.js
+++ models/flight.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport:{type:String, enum:['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
+    arrival: {type:Date}
+}, {timestamps:true})
+
 const flightScehma = new Schema({
     airline:{type:String, required:true, enum:['American', 'Southwest', 'United']},
     airport:{type:String, required:true, enum:['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default:'DEN'},
     flightNo:{type:Number, required:true, min:10, max:9999},
     departs:{type:Date, 
         default: function () {
-            return new Date().getFullYear();
-        }}
-});
-
-module.exports = mongoose.model('Movie', flightScehma);
+            return new Date().getFullYear() + 1;
+        }},
+    destinations:[destinationSchema]
+}, {timestamps:true});
+module.exports = mongoose.model('Flight', flightScehma);
