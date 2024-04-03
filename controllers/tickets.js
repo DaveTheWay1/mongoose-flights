9728ffbd176b776a15acdda4e1f5ca32d5c333db
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-03 08:43:35 -0700

    corrected functionlity performing the same behaviors and included a delete ticket functionality

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,20 +1,36 @@
 const Flight = require('../models/flight');
+const Ticket = require('../models/ticket')
 
 module.exports={
-    addTicket,
-    new:newTicket
+    new:newTicket,
+    addNewTicket,
+    delete:deleteTicket
 }
 
 async function newTicket(req,res){
-    const flight = await Flight.findById(req.params.id);
+    const flight = await Flight.findById(req.params.id)
     res.render('tickets/new', {flight})
 }
 
-async function addTicket(req,res){
-    console.log('here')
-    console.log(req.body)
-    const flight = await Flight.findById(req.params.id);
-    flight.tickets.push(req.body);
-    await flight.save()
-    res.redirect(`/flights/${flight.id}`)
+async function addNewTicket(req,res){
+    try {
+        console.log(req.body)
+        const { seat, price, flight } = req.body;
+        const ticket = new Ticket({ seat, price, flight });
+        await ticket.save();
+        console.log(ticket)
+        console.log('here')
+        res.redirect(`/flights/${flight}`)
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+async function deleteTicket(req,res){
+    const ticket = await Ticket.findByIdAndDelete(req.params.id)
+    const flight_id = ticket.flight
+    res.redirect(`/flights/${flight_id}`)
+}
+
+
+
